c25b5bbd690f1b635007584dad51c96d150f67c7
Rob Johnston <rob.johnston@joyent.com>
2019-08-14 17:20:23 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # topo-digraph-xml
-This is a Rust crate that defines a set of structures for deserialzing the XML definition of a libopo Directed Graph topology.
+This is a tiny Rust crate that simply defines a set of structures for
+deserializing the XML definition of a libopo Directed Graph topology.
+
+Today this XML is produced by either running  ```/usr/lib/fm/fmd/sastopo -x``` on SmartOS
+or by leveraging the topo_digraph_serialize() interface in libtopo.
+
+**NOTE**: Both the ```sastopo``` tool and the ```topo_digraph_serialize()``` interface
+are only available in the development "sastopo" branch of
+[git://github.com/illumos-joyent](git://github.com/illumos-joyent).
+
---
+++ src/lib.rs
@@ -0,0 +1,79 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright 2019 Joyent, Inc.
+//
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_xml_rs;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "nvpair")]
+pub struct NvlistXmlArrayElement {
+    #[serde(rename = "nvpair")]
+    pub nvpairs: Vec<NvpairXML>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "nvpair")]
+pub struct NvpairXmlArrayElement {
+    pub value: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "nvpair")]
+pub struct NvpairXML {
+    pub name: Option<String>,
+    #[serde(rename = "type")]
+    pub nvtype: Option<String>,
+    pub value: Option<String>,
+    pub nelem: Option<String>,
+    #[serde(rename = "nvpair")]
+    pub nvpair_elements: Option<Vec<NvpairXmlArrayElement>>,
+    #[serde(rename = "nvlist")]
+    pub nvlist_elements: Option<Vec<NvlistXmlArrayElement>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "edge")]
+pub struct TopoEdgeXML {
+    pub fmri: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "outgoing-edges")]
+pub struct TopoOutgoingEdgesXML {
+    pub nelem: String,
+    #[serde(rename = "edge")]
+    pub edges: Vec<TopoEdgeXML>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "vertex")]
+pub struct TopoVertexXML {
+    pub name: String,
+    pub instance: String,
+    pub fmri: String,
+    #[serde(rename = "nvpair")]
+    pub propgroups: Vec<NvpairXML>,
+    #[serde(rename = "outgoing-edges")]
+    pub outgoing_edges: Option<TopoOutgoingEdgesXML>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "nvpair")]
+pub struct TopoVerticesXML {
+    pub nelem: String,
+    pub vertex: Vec<TopoVertexXML>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename = "topo_digraph")]
+pub struct TopoDigraphXML {
+    #[serde(rename = "fmri-scheme")]
+    pub scheme: String,
+    pub vertices: TopoVerticesXML,
+}
